a41135b4bfaafbdd0d683192bb2948d5376c1969
bsmith86dev <bsmith86.dev@gmail.com>
2023-07-31 21:08:49 -0600

    ChatScreen setup

---
+++ App.js
@@ -0,0 +1,39 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+// 👇 Import our component
+import ChatListItem from "./src/components/ChatListItem";
+import ChatsScreen from "./src/screens/ChatScreen";
+
+const chat = {
+  id: "1",
+  user: {
+    image:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg",
+    name: "Lukas",
+  },
+  lastMessage: {
+    text: "Oke",
+    createdAt: "07:30",
+  },
+};
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* 👇 Render a ChatListItem */}
+      <ChatsScreen/>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 50,
+  },
+});
---
+++ src/components/ChatListItem/index.js
@@ -0,0 +1,65 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
+
+const ChatListItem = ({ chat }) => {
+  console.log(chat);
+  return (
+    <View style={styles.container}>
+			{/* User Avatar */}
+      <Image
+        source={{
+          uri: chat.user.image,
+        }}
+        style={styles.image}
+      />
+
+			{/* Content Container */}
+      <View style={styles.content}>
+				{/* Row */}
+				<View style={styles.row}>
+	        <Text style={styles.name} numbersOfLines={1}>{chat.user.name}</Text>
+		      <Text style={styles.subTitle}>{dayjs(chat.lastMessage.createdAt).fromNow(true)}</Text>
+	      </View>
+
+        <Text style={styles.subTitle} numberOfLines={2}>{chat.lastMessage.text}
+</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "stretch",
+    marginHorizontal: 10,
+    marginVertical: 5,
+    height: 70,
+  },
+  image: {
+    width: 60,
+    height: 60,
+    aspectRatio: 1,
+    borderRadius: 30,
+    marginRight: 10,
+  },
+  content: {
+    flex: 1,
+    borderBottomColor: "lightGray",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  row: {
+    flexDirection: "row",
+    marginBottom: 5,
+  },
+  name: {
+    flex: 1,
+  },
+  subTitle: {
+    color: "grey",
+  },
+});
+
+export default ChatListItem;
